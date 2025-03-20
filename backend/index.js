c6f71cbb6b36da0754036d94d79e606757b7ff3d
Yong Le He <ygle.he@gmail.com>
2025-03-20 18:16:08 -0400

    feat: implement most portfolio endpoints

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,15 @@
 import express from "express";
+import { portfolio } from "./routes/portfolio.js";
+import { user } from "./routes/user.js";
 const app = express();
+const port = process.env.PORT || "3000";
 
 app.use(express.json());
 
 // Add routers from /routers here
+app.use("/portfolio", portfolio)
+app.use("/user", user);
 
-app.listen(process.env.PORT || "3000", () => {
+app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
---
+++ backend/routes/portfolio.js
@@ -0,0 +1,320 @@
+import { Router } from "express";
+import { query, getClient } from "../db/index.js";
+
+export const portfolio = Router();
+
+/** Username creates a new portfolio with name slname. */
+portfolio.post("/create", async (req, res) => {
+  const { username, pname } = req.body;
+
+  console.log(username, pname);
+
+  const client = await getClient();
+  client.query("BEGIN");
+  try {
+    const result = await client.query(
+      `
+      INSERT INTO folder (folder_name)
+      VALUES ($1)
+      RETURNING *
+    `,
+      [pname]
+    );
+    if (result.rowCount === 0) throw Error;
+
+    const fid = result.rows[0].fid;
+
+    if (!fid) throw Error;
+
+    await client.query(
+      `
+      INSERT INTO creates
+      VALUES ($1, $2)
+    `,
+      [username, fid]
+    );
+    await client.query(
+      `
+      INSERT INTO portfolio
+      VALUES ($1, 0)
+    `,
+      [fid]
+    );
+
+    client.query("COMMIT");
+    res.json({ pid: fid });
+  } catch {
+    client.query("ROLLBACK");
+    res.json({ pid: -1 }).status(400);
+  } finally {
+    client.release();
+  }
+});
+
+/** Username views all portfolios created by them. */
+portfolio.get("/view/all/:username", async (req, res) => {
+  const username = req.params.username;
+
+  try {
+    const result = await query(
+      `
+      SELECT folder_name FROM
+      (
+        (
+          (SELECT fid FROM Creates WHERE username = $1)
+          NATURAL JOIN Folder
+        )
+        JOIN Portfolio
+        ON fid = pid 
+      )
+      `,
+      [username]
+    );
+
+    res.json(result.rows.map(({ folder_name }) => folder_name));
+  } catch {
+    res.json([]).status(404);
+  }
+});
+
+/** Username views the portfolio represended by pid. Returns the portfolio name,
+ * cash, and all stock holdings.
+ */
+portfolio.get("/view/one/:username/:pid", async (req, res) => {
+  const username = req.params.username;
+  const pid = req.params.pid;
+
+  try {
+    const result = await query(
+      `
+    SELECT * FROM (
+      (
+      (SELECT * FROM Creates c WHERE username = $1 AND c.fid = $2)
+      NATURAL JOIN Folder f JOIN Portfolio p ON f.fid = p.pid
+      )
+      NATURAL LEFT JOIN Stockholding
+    )
+    `,
+      [username, pid]
+    );
+
+    if (result.rowCount === 0)
+      throw Error("This porfolio does not belong to this user.");
+
+    res.json({
+      pid: result.rows[0].pid,
+      folder_name: result.rows[0].folder_name,
+      amount: result.rows[0].amount,
+      stocks: result.rows
+        .filter(({ symbol, share }) => symbol !== null && share !== null)
+        .map(({ symbol, share }) => ({ symbol: symbol, share: share })),
+    });
+  } catch (e) {
+    console.log(e);
+    res.json({}).status(400);
+  }
+});
+
+/** Username withdraws cash into an external bank account */
+portfolio.put("/withdraw/:username/:pid", async (req, res) => {
+  const username = req.params.username;
+  const pid = req.params.pid;
+  const { amount } = req.body;
+
+  try {
+    const result = await query(
+      `
+      UPDATE portfolio
+      SET amount = amount - $3
+      WHERE pid = $2 AND EXISTS (
+        SELECT fid from Creates WHERE fid = $2 AND username = $1
+      )
+      `,
+      [username, pid, amount]
+    );
+
+    if (result.rowCount === 0) throw Error;
+
+    res.json({ success: true });
+  } catch {
+    res.json({ success: false }).status(400);
+  }
+});
+
+/** Username deposits cash into pid1 from pid2 */
+portfolio.put("/deposit/:username/:pid1/:pid2", async (req, res) => {
+  const username = req.params.username;
+  const pid1 = req.params.pid1;
+  const pid2 = req.params.pid2;
+  const { amount } = req.body;
+
+  const client = await getClient();
+  await client.query("BEGIN");
+  try {
+    if (
+      (
+        await client.query(
+          `
+      SELECT * FROM Creates
+      WHERE (fid=$2 OR fid=$3) AND username=$1
+      `,
+          [username, pid1, pid2]
+        )
+      ).rowCount !== 2
+    )
+      throw Error(
+        `${username} is not the creator of portfolio ${pid1} or ${pid2}`
+      );
+
+    if (
+      (
+        await client.query(
+          `
+      UPDATE Portfolio
+      SET amount = amount + $2
+      WHERE pid = $1
+      `,
+          [pid1, amount]
+        )
+      ).rowCount === 0
+    )
+      throw Error(`Failed to add amount into ${pid1}`);
+
+    if (
+      (
+        await client.query(
+          `
+      UPDATE Portfolio
+      SET amount = amount - $2
+      WHERE pid = $1
+      `,
+          [pid2, amount]
+        )
+      ).rowCount === 0
+    )
+      throw Error(`Failed to subtract amount into ${pid2}`);
+
+    client.query("COMMIT");
+    res.json({ success: true });
+  } catch (e) {
+    console.log(e);
+    client.query("ROLLBACK");
+    res.json({ success: false }).status(400);
+  } finally {
+    client.release();
+  }
+});
+
+/** Username deposits cash into pid from an external bank account */
+portfolio.put("/deposit/:username/:pid", async (req, res) => {
+  const username = req.params.username;
+  const pid = req.params.pid;
+  const { amount } = req.body;
+
+  try {
+    const result = await query(
+      `
+      UPDATE portfolio
+      SET amount = amount + $3
+      WHERE pid = $2 AND EXISTS (
+        SELECT fid from Creates WHERE fid = $2 AND username = $1
+      )
+      `,
+      [username, pid, amount]
+    );
+
+    if (result.rowCount === 0) throw Error;
+
+    res.json({ success: true });
+  } catch {
+    res.json({ success: false }).status(400);
+  }
+});
+
+/** Username adds shares of stock into folder */
+portfolio.post("/buy", async (req, res) => {
+  const { username, pid, symbol, shares } = req.body;
+  const client = await getClient();
+
+  await client.query("BEGIN");
+  try {
+    // Make sure username is the creator of pid
+    if (
+      (
+        await client.query(
+          `
+      SELECT * FROM Creates
+      WHERE fid=$2 AND username=$1
+      `,
+          [username, pid]
+        )
+      ).rowCount == 0
+    )
+      throw Error(`${username} is not the creator of portfolio ${pid}`);
+
+    // Check if this portfolio already holds the share
+    if (
+      (await client.query(
+        `
+      UPDATE Stockholding
+      SET share = share + $3
+      WHERE fid = $1 AND symbol = $2
+      `,
+        [pid, symbol, shares]
+      ).rowCount) === 0
+    ) {
+      await client.query(
+        `
+      INSERT INTO Stockholding
+      VALUES ($1, $2, $3)
+      `,
+        [pid, symbol, shares]
+      );
+    }
+
+    const price = await client.query(
+      `
+      SELECT close FROM (
+        Stockdata s1 NATURAL JOIN (
+          SELECT s2.symbol, MAX(s2.time_stamp)
+          FROM Stockdata s2
+          WHERE s2.symbol = $1
+          GROUP BY symbol 
+        )
+      )
+        `,
+      [symbol]
+    );
+    if (price.rowCount === 0)
+      throw Error(`Could not find stock price for ${symbol}`);
+
+    if (
+      (
+        await client.query(
+          `
+      UPDATE Portfolio
+      SET amount = amount - $1
+      WHERE pid = $2
+      `,
+          [price.rows[0].close * shares, pid]
+        )
+      ).rowCount === 0
+    )
+      throw Error(`Failed to update`);
+
+    await client.query("COMMIT");
+    res.json({ success: true });
+  } catch (e) {
+    console.error(e);
+    await client.query("ROLLBACK");
+    res.json({ success: false });
+  } finally {
+    client.release();
+  }
+});
+
+/** Gets the market value of the portfolio. That is, the cash + value of all stocks. */
+portfolio.get("/value/:username/:pid", async (req, res) => {});
+
+/** Username updates their review for slid. */
+portfolio.get("/performance/past/:symbol/:interval", async (req, res) => {});
