17ff7d03dfdc8a7e55b682db288c8aba89f86a6f
AlvinCao88 <alvincao88@gmail.com>
2025-03-22 20:28:30 -0400

    merge conflicted resovled

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,17 @@
 import express from "express";
-<<<<<<< HEAD
-import Routes from "./routes/index.js"
-=======
 import { portfolio } from "./routes/portfolio.js";
 import { user } from "./routes/user.js";
 import { stock } from "./routes/stock.js";
->>>>>>> yongle
 const app = express();
-const port = process.env.PORT || "3000";
 
 const port = process.env.PORT || "3000";
 
 app.use(express.json());
 
 // Add routers from /routers here
-<<<<<<< HEAD
-app.use("/", Routes);
-=======
 app.use("/portfolio", portfolio);
 app.use("/stock", stock);
 app.use("/user", user);
->>>>>>> yongle
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
