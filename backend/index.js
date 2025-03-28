871e116b2199912656c693da8d4c423b2eb4f662
Yong Le He <ygle.he@gmail.com>
2025-03-27 20:07:50 -0400

    chore: add cors

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,17 @@
 import express from "express";
+import cors from "cors";
 import { portfolio } from "./routes/portfolio.js";
 import { user } from "./routes/user.js";
 import { stock } from "./routes/stock.js";
 import { friend } from "./routes/friend.js";
-import { stocklist } from "./routes/stocklist.js"
+import { stocklist } from "./routes/stocklist.js";
 
 const app = express();
 
 const port = process.env.PORT || "8080";
 
 app.use(express.json());
+app.use(cors());
 
 // Add routers from /routers here
 app.use("/portfolio", portfolio);
